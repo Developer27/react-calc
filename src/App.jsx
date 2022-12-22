6c4d1daae5ecb83d64d827a401b8a1b8dc9cf041
Dmitry <dyivanov32@gmail.com>
2022-12-22 02:17:10 +0200

    First look design

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import CalcButton from './components/CalcButton/CalcButton';
+import './app.css';
+
+function App() {
+  return (
+   <div className="container">
+    <div className="main">
+        <input type="text" className='search_inp' />
+      <div className="numbers">
+        <div className="numbers_row">
+          <CalcButton number={7}/>
+          <CalcButton number={8}/>
+          <CalcButton number={9}/>
+          <CalcButton number={'x'}/>
+        </div>
+        <div className="numbers_row">
+          <CalcButton number={4}/>
+          <CalcButton number={5}/>
+          <CalcButton number={6}/>
+          <CalcButton number={'-'}/>
+        </div>
+        <div className="numbers_row">
+          <CalcButton number={1}/>
+          <CalcButton number={2}/>
+          <CalcButton number={3}/>
+          <CalcButton number={'+'}/>
+        </div>
+        <div className="numbers_row">
+          <CalcButton number={0}/>
+          <CalcButton number={','}/>
+          <CalcButton number={'='}/>
+        </div>
+      </div>
+    </div>
+   </div>
+  );
+}
+
+export default App;
